7bed7f9daf5900f7ba34286e59872ed8484b7302
Ketul-at-776632819409 <k2lsuthar@gmail.com>
2020-11-01 20:53:51 -0500

    Added load_configuration function

---
+++ src/auction/util.py
@@ -0,0 +1,13 @@
+"""
+created_by : Ketulkumar Suthar
+creatde_date : 1st
+"""
+from SortableChallenge.src.auction.settings import CONFIG_FILE_PATH
+
+
+def load_configuration():
+    """
+    This function will load config file
+    :return:
+    """
+    pass

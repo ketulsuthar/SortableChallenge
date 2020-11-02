854815d7fb99ca51dc322db258fd4f5d2ff8bfa5
Ketul-at-776632819409 <k2lsuthar@gmail.com>
2020-11-01 22:08:27 -0500

    Added logic for getting data fro sites and bidders

--- src/main.py
+++ src/main.py
@@ -2,7 +2,14 @@
 create_by : Ketulkumar Suthar
 created_date : 1st November 2020
 """
-
+from SortableChallenge.src.auction.util import load_configuration, get_bidders, get_sites
 
 if __name__ == "__main__":
-    pass
+    # load config file
+    sites_bidders = load_configuration()
+    if sites_bidders:
+        # filter all the sites
+        sites = get_sites(sites_bidders.get('sites',[]))
+        # filter all the bidders
+        bidders = get_bidders(sites_bidders.get('bidders',[]))
+

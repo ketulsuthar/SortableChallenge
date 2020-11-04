b99e65e5794408ecf3b37f6bbf280c0824757bae
Ketul-at-776632819409 <k2lsuthar@gmail.com>
2020-11-04 09:13:32 -0500

    updated main code loading config file

--- src/main.py
+++ src/main.py
@@ -2,14 +2,21 @@
 create_by : Ketulkumar Suthar
 created_date : 1st November 2020
 """
-from SortableChallenge.src.auction.util import load_configuration, get_bidders, get_sites
+from SortableChallenge.src.auction.util import load_configuration, get_bidders, get_sites, is_valid_config, get_input,is_valid_input
 
 if __name__ == "__main__":
     # load config file
     sites_bidders = load_configuration()
-    if sites_bidders:
+    if is_valid_config(sites_bidders):
         # filter all the sites
         sites = get_sites(sites_bidders.get('sites',[]))
         # filter all the bidders
         bidders = get_bidders(sites_bidders.get('bidders',[]))
+
+        auctions = get_input()
+        print(auctions)
+        if is_valid_input(auctions):
+            for auction in auctions:
+                print(auction)
+
 

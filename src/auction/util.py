8634f69193d7e17ea1bd38f3d6a6fe01317c6eb5
Ketul-at-776632819409 <k2lsuthar@gmail.com>
2020-11-04 09:14:01 -0500

    updated util file

--- src/auction/util.py
+++ src/auction/util.py
@@ -4,23 +4,73 @@ creatde_date : 1st
 """
 import sys
 import json
-from SortableChallenge.src.auction.settings import CONFIG_FILE_PATH
+import jsonschema
+from jsonschema import validate
+from SortableChallenge.src.auction.settings import CONFIG_FILE_PATH, SCHEMA_CONFIG_PATH, SCHEMA_INPUT_PATH
 
+
+def read_file(type):
+    """
+
+    :param type:
+    :return:
+    """
+    try:
+        file_name =''
+        if type.upper() == "CONFIG":
+            file_name = CONFIG_FILE_PATH
+        elif type.upper() == "SCHEMA_CONFIG":
+            file_name = SCHEMA_CONFIG_PATH
+        elif type.upper() == "SCHEMA_INPUT":
+            file_name = SCHEMA_INPUT_PATH
+
+
+        print(file_name)
+        if file_name:
+            with open(file_name,'r') as f:
+                return json.load(f)
+        else:
+            print(f"Error : Check config file path. [load_configuration]")
+    except Exception as err:
+        print(f"Error : {err} [read_file]")
 
 def load_configuration():
     """
     This function will load config file
     :return:
     """
+    return read_file('CONFIG')
+
+def is_valid_config(sites_bidders):
+    """
+
+    :param sites_bidders:
+    :param type:
+    :return:
+    """
+    config_schema = read_file('SCHEMA_CONFIG')
     try:
-        if CONFIG_FILE_PATH:
-            with open(CONFIG_FILE_PATH,'r') as f:
-                print(f)
-                return json.load(f)
-        else:
-            print(f"Error : Check config file path. [load_configuration]")
-    except Exception as err:
-        print(f"Error : {err} [load_configuration]")
+        validate(sites_bidders, config_schema)
+    except jsonschema.exceptions.ValidationError as err:
+        print(f"Error : {err} [is_valid_config]")
+        return False
+    else:
+        return True
+
+def is_valid_input(auctions):
+    """
+
+    :param auctions:
+    :return:
+    """
+    input_schema = read_file('SCHEMA_INPUT')
+    try:
+        validate(auctions, input_schema)
+    except jsonschema.exceptions.ValidationError as err:
+        print(f"Error : {err} [is_valid_input]")
+        return False
+    else:
+        return True
 
 
 def get_sites(sites_bidders):
@@ -50,3 +100,11 @@ def get_bidders(sites_bidders):
             'adjustment' : bidder['adjustment']
         }
     return bidders
+
+
+def get_input():
+    """
+
+    :return:
+    """
+    return json.loads(''.join(sys.stdin.readlines()))

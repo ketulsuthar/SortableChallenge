f9107318f637a09b4c8eebdb14298a2cfcb8aa4c
Ketul-at-776632819409 <k2lsuthar@gmail.com>
2020-11-04 16:06:33 -0500

    Updated setting file

--- src/auction/settings.py
+++ src/auction/settings.py
@@ -2,6 +2,13 @@
 created_by : Ketulkumar suthar
 created_date : 1st November 2020
 """
+import os
 import pathlib
 
 CONFIG_FILE_PATH = pathlib.Path(__file__).parent / 'config.json'
+
+
+SCHEMA_CONFIG_PATH = pathlib.Path(__file__).parent.parent / 'schema/config_schema.json'
+SCHEMA_INPUT_PATH = pathlib.Path(__file__).parent.parent / 'schema/input_schema.json'
+
+    #os.path.join(os.path.dirname(__file__),'../schema/config_schema.json')
